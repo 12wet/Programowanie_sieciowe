a9936dac4ecbf20617323dfabda7a1d6f1c077cc
TwelveWet <62149776+12wet@users.noreply.github.com>
2022-06-13 03:09:27 +0200

    updated MemberController

--- Zadanie4/src/main/java/com/exercise4/MemberController.java
+++ Zadanie4/src/main/java/com/exercise4/MemberController.java
@@ -1,11 +1,11 @@
 package com.exercise4;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-@RestController
+@Configuration
 public class MemberController {
 
     @Bean
